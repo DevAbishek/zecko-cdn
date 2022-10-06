b0bc828bb2606f1c24c8376e8c9239bf9a2fab8d
Abishek Agarwal <disk.abishek@gmail.com>
2022-10-07 00:30:55 +0530

    cp amp code

--- amp.js
+++ amp.js
@@ -1,4 +1,7 @@
-const p = document.createElement('p');
-p.textContent = 'A second line of text';
-document.body.appendChild(p);
-console.log('jkjk')
+const button = document.getElementById('hello-url');
+
+button.addEventListener('click', () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello World!';
+  document.body.appendChild(h1);
+});
