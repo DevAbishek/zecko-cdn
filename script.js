1f35f756144a0f6e31596857d2c10e6a66b1d79f
Abishek Agarwal <disk.abishek@gmail.com>
2022-10-11 21:41:50 +0530

    add again

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ console.log('jiji')
 const script2 = 'https://cdn.jsdelivr.net/gh/DevAbishek/zecko-cdn/script2.js'
 
 function loadScript(scriptURL) {
+    console.log('kmk')
     var script = document.createElement('script');
     script.async = true;
     script.src = scriptURL;
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    console.log('done')
 }
 loadScript(script2);
+console.log('done2')
