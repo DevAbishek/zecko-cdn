8bb7e5e5f9e6789728dd84c5da0662b0ad7b67c4
Abishek Agarwal <disk.abishek@gmail.com>
2022-10-11 21:33:26 +0530

    add script, script2

---
+++ workingreducedcode.js
@@ -0,0 +1,152 @@
+<!doctype html>
+<html ⚡>
+<head>
+<meta charset="utf-8">
+    <title>amp-script</title>
+    <link rel="canonical" href="self.html">
+    <meta name="viewport" content="width=device-width">
+    <style amp-boilerplate>
+      body {
+        -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+      }
+      @-webkit-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-moz-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-ms-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-o-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+    </style>
+    <noscript><style amp-boilerplate>
+        body {
+          -webkit-animation: none;
+          -moz-animation: none;
+          -ms-animation: none;
+          animation: none;
+        }
+      </style></noscript>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <script async custom-element="amp-script" src="https://cdn.ampproject.org/v0/amp-script-0.1.js"></script>
+
+    <meta name="amp-script-src" content="sha384-11AF1JbgyDpWrzxui13z39qpAsp0rX93TQlM28Zkz_gsVakj9ngmYQuBzPZMasqZ sha384-iER2Cy-P1498h1B-1f3ngpVEa9NG1xIxKqg0rNkRX0e7p5s0GYdit1MRKsELIQe8 sha384-UPY0FmlOzIjSqWqMgbuaEbqIdvpGY_FzCuTAyoLdrFJb2NYf8cPWJlugA0rUbXjL
+    sha384-tvoKljCwTSMId8wpKdpz8LLZ0LX4CYPO5SHE3naqBMSpjiAXs7nqAFyDqcIyKLnu
+    sha384-28AuuOPww1R3yKdaPfiLgMoR1IpWmqwZNYtMnTVwAJuPX_HpIe6qSXPBC-E5I0Sd
+    sha384-fZQaBxFzBabP2yhhZpkYc4zA35clMkPK72glImLl0jGgLy_m9VjzZVhOdGJ52uhK sha384-PiY9aRrCB1owHsHfK1Jtc9szpEHPl0Liqhn-ZdN1GKt_KIeoTH_Nbd39CgBA2feJ sha384-0H2RUoUOsS8PCwM3cHlHz5fRzM6LcycOMzgtZJTFuJ6CKHzHmA-ibK-R1fsza8NW">
+
+<title>amp-script / 6</title>
+<style amp-custom>
+      .sample {
+        font-family: 'Noto Sans', sans-serif;
+        font-size: 16px;
+        padding: 8px;
+      }
+
+      .sample button {
+        background-color: #005af0;
+        font-weight: 700;
+        color: white;
+        border: none;
+        outline: none;
+        border-radius: 12px;
+        padding: 8px 16px;
+        margin: 8px 0;
+      }
+
+      .sample button[disabled] {
+        background-color: #ccc;
+        color: #666;
+      }
+
+      #validated-input {
+        height: 26px;
+        width: 225px;
+        padding: 6px;
+        box-sizing: border-box;
+      }
+
+      .answer-text {
+        color: #555;
+        font-weight: bold;
+      }
+
+      #translated-greeting {
+        margin: 0;
+        padding-left: 4px;
+        color: #555;
+      }
+
+      .fruit-button {
+        position: relative;
+        top: -60px;
+      }
+
+      #carousel-sample p {
+        margin-left: 16px;
+      }
+
+      #carousel-button {
+        top: -100px;
+      }
+
+      #live-blog-start {
+        margin-top: 0;
+      }
+
+      #live-blog-area {
+        border: 2px solid #ccc;
+        height: 150px;
+        overflow-y: scroll;
+      }
+
+      #live-blog-area .time {
+        color: #666;
+        font-weight: bold;
+      }
+
+</style>
+<meta name="robots" content="noindex, nofollow">
+</head>
+<body>
+
+
+    <amp-script layout="container" src="https://cdn.jsdelivr.net/gh/DevAbishek/zecko-cdn/reducedcode.js" class="sample">
+      <button id="hello-url">Say hello!</button>
+    </amp-script>
+</body>
+</html>
